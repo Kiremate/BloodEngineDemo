d9ea2384e77241ff7d8e574dff6dc4377ee93f05
bloodmask <kiremate@hotmail.com>
2023-01-18 01:14:56 +0100

    Redefined Blood Engine, Restructured Folders and created Adio System and bunch other classes.

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,74 +1,71 @@
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
- *                                                                             *
- *  Este código es de dominio público.                                         *
- *  angel.rodriguez@esne.edu                                                   *
- *  2015.04                                                                    *
- *                                                                             *
-\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 #include <SDL.h>
 #include <ciso646>
-
+#include <iostream>
+#include "Scene.h"
+#include "Render_System.h"
+#include "Audio_System.h"
+#include "Physics_System.h"
 int main(int, char**)
 {
-    // Se hace inicializa SDL:
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        SDL_Log("No se ha podido inicializar SDL2.");
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
+        return 1;
     }
-    else
-    {
-        // Se crea una ventana:
-
-        SDL_Window* window = SDL_CreateWindow("SDL2 Window Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 300, 600, SDL_WINDOW_SHOWN);
-
-        if (not window)
-        {
-            SDL_Log("No se ha podido crear una ventana.");
-        }
-        else
-        {
-            // Se toma el buffer de la ventana para dibujar en él:
-
-            SDL_Surface* surface = SDL_GetWindowSurface(window);
-
-            if (not surface)
-            {
-                SDL_Log("No se ha podido acceder al buffer de la ventana.");
-            }
-            else
-            {
-                bool exit = false;
-
-                do
-                {
-                    // Se procesan los eventos acumulados:
-
-                    SDL_Event event;
-
-                    while (SDL_PollEvent(&event) > 0)
-                    {
-                        if (event.type == SDL_QUIT)
-                        {
-                            exit = true;
-                        }
-                    }
-
-                    // Se rellena el fondo de la ventana de color gris:
-
-                    SDL_FillRect(surface, 0, SDL_MapRGB(surface->format, 225, 225, 225));
-
-                    SDL_UpdateWindowSurface(window);
-                } while (!exit);
-            }
-
-            SDL_DestroyWindow(window);
-        }
-
-        SDL_Quit();
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("My Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+    if (!window) {
+        std::cerr << "Error creating window: " << SDL_GetError() << std::endl;
+        return 1;
     }
 
+    // Create a scene
+    blood_engine::Scene scene(*window);
+
+    // Prepare the Systems
+    // Render
+    std::unique_ptr<blood_engine::Render_System> render_system =
+        std::unique_ptr<blood_engine::Render_System>();
+    // Physics
+    std::unique_ptr<blood_engine::Physics_System> physics_system =
+        std::unique_ptr<blood_engine::Physics_System>();
+    // Audio
+    std::unique_ptr<blood_engine::Audio_System> audio_system =
+        std::unique_ptr<blood_engine::Audio_System>();
+
+    scene.AddSystem(std::move(render_system));
+    scene.AddSystem(std::move(physics_system));
+    scene.AddSystem(std::move(audio_system));
+	
+    // Create a new Kernel
+    blood_engine::Kernel kernel;
+	
+    // Run the kernel
+    kernel.Run(&scene);
+	
+    // Main loop
+    bool running = true;
+    while (running) {
+        // Handle events
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // Update the scene
+        scene.Update();
+        
+        // Render the scene
+        scene.Render();
+
+        // Swap buffers
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Clean up
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
